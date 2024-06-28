f4ff2262ae09876d57f02f82e45052d58ec92c3e
khawlaks <keqqat20@gmail.com>
2024-06-28 10:20:30 +0100

    export excel in affectation

---
+++ assets/affectation/export-csv-affectation.php
@@ -0,0 +1,30 @@
+<?php include '../../db/connexion.php';
+
+
+$output=fopen('php://output','w');
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=export-affectation.csv');
+header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.
+
+fputcsv($output,array('Identification d affectation','WorkOrderNumber','Nom de Ressource'));
+
+$sql= "SELECT w.id as workorder_id, w.WorkOrderNumber, GROUP_CONCAT(r.nameressource SEPARATOR ', ') AS ressources 
+                    FROM affectations a 
+                    JOIN workorders w ON w.id = a.WorkOrderID 
+                    JOIN ressouce r ON r.id = a.RessourceID 
+                    GROUP BY w.id 
+                    ORDER BY r.nameressource DESC";
+$result=$conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        fputcsv($output,$row);
+    }
+}
+fclose($output);
+$conn->close();
+
+
+
+
+?>
