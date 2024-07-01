300333073c10d9172c6cdc32212600db5cfbc7b8
khawlaks <keqqat20@gmail.com>
2024-07-01 14:39:18 +0100

    ajouter le diagramme gantt

--- charts/pie-projets.php
+++ charts/pie-projets.php
@@ -2,6 +2,7 @@
 include '../../db/connexion.php';
 
 $query = "SELECT Status, COUNT(*) as count FROM projects GROUP BY Status";
+
 $result = $conn->query($query);
 
 $Status = [];
@@ -12,7 +13,28 @@ while ($row = $result->fetch_assoc()) {
     $data[] = $row['count'];
 }
 
-// Convertir les tableaux en chaînes JSON
-$StatusJSON = json_encode($Status);
+// Convert the arrays to JSON strings
+$labelsJSON = json_encode($Status);
 $dataJSON = json_encode($data);
+
+echo "<script>console.log('labelsJSON: " . addslashes($labelsJSON) . "');</script>";
+echo "<script>console.log('dataJSON: " . addslashes($dataJSON) . "');</script>";
+
+
+
+// Gantt
+
+
+$sql = "SELECT ProjectName, Description, StartDate, EndDate FROM projects";
+$result = $conn->query($sql);
+
+$projects = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $projects[] = $row;
+    }
+}
+$conn->close();
+
+$projectsJSON = json_encode($projects);
 ?>
